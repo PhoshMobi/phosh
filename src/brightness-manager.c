PhoshMobi/phosh

commit 57ee80353d5f7877d33ffd0e85be9dce66d3e7cd

brightness-manager: Add Brightness DBus interface
The DBus API XML is from gnome-shell as of:

  e54aa588c ("slider: Use the sprite of the touch event, not the pointer")

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1785>

src/brightness-manager.c
@@ -20,6 +20,8 @@
 #define KEYBINDING_KEY_BRIGHTNESS_UP_MONITOR "screen-brightness-up-monitor"
 #define KEYBINDING_KEY_BRIGHTNESS_DOWN_MONITOR "screen-brightness-down-monitor"
 
+#define POWER_SCHEMA "org.gnome.settings-daemon.plugins.power"
+
 /**
  * PhoshBrightnessManager:
  *
@@ -29,7 +31,7 @@
 #define BRIGHTNESS_STEP_AMOUNT(max) ((max) < 20 ? 1 : (max) / 20)
 
 struct _PhoshBrightnessManager {
-  GObject               parent;
+  PhoshDBusBrightnessSkeleton parent;
 
   GStrv           action_names;
   GSettings      *settings;
@@ -37,8 +39,118 @@ struct _PhoshBrightnessManager {
   GtkAdjustment  *adjustment;
   gulong          value_changed_id;
   gboolean        setting_brightness;
+
+  GSettings      *settings_power;
+  gboolean        dimmed;
+  int             dbus_name_id;
+  double          saved_brightness;
 };
-G_DEFINE_TYPE (PhoshBrightnessManager, phosh_brightness_manager, G_TYPE_OBJECT)
+
+static void phosh_brightness_manager_brightness_init (PhoshDBusBrightnessIface *iface);
+
+G_DEFINE_TYPE_WITH_CODE (PhoshBrightnessManager,
+                         phosh_brightness_manager,
+                         PHOSH_DBUS_TYPE_BRIGHTNESS_SKELETON,
+                         G_IMPLEMENT_INTERFACE (PHOSH_DBUS_TYPE_BRIGHTNESS,
+                                                phosh_brightness_manager_brightness_init))
+
+
+static void
+on_name_acquired (GDBusConnection *connection, const char *name, gpointer user_data)
+{
+  g_debug ("Acquired name %s", name);
+}
+
+
+static void
+on_name_lost (GDBusConnection *connection, const char *name, gpointer user_data)
+{
+  g_debug ("Lost or failed to acquire name %s", name);
+}
+
+
+static void
+on_bus_acquired (GDBusConnection *connection, const char *name, gpointer user_data)
+{
+  g_autoptr (GError) err = NULL;
+  PhoshBrightnessManager *self = PHOSH_BRIGHTNESS_MANAGER (user_data);
+
+  /* We need to use GNOME Shell's object path here to make g-s-d happy */
+  if (!g_dbus_interface_skeleton_export (G_DBUS_INTERFACE_SKELETON (self),
+                                         connection,
+                                         "/org/gnome/Shell/Brightness",
+                                         &err)) {
+    g_warning ("Failed export brightness interface: %s", err->message);
+  }
+}
+
+
+static gboolean
+phosh_brightness_manager_handle_set_auto_brightness_target (PhoshDBusBrightness   *object,
+                                                            GDBusMethodInvocation *invocation,
+                                                            gdouble                arg_target)
+{
+  g_debug ("Target brightness: %f", arg_target);
+
+  /* TODO: Leave unimplemented until
+   * https://gitlab.gnome.org/GNOME/gnome-settings-daemon/-/merge_requests/442
+   * is fixed as we otherwise might end up with a very dark screen */
+
+  phosh_dbus_brightness_complete_set_auto_brightness_target (object, invocation);
+  return TRUE;
+}
+
+
+static gboolean
+phosh_brightness_manager_handle_set_dimming (PhoshDBusBrightness   *object,
+                                             GDBusMethodInvocation *invocation,
+                                             gboolean               arg_enable)
+{
+  PhoshBrightnessManager *self = PHOSH_BRIGHTNESS_MANAGER (object);
+  double target;
+
+  g_debug ("Dimming: %s", arg_enable ? "enabled" : "disabled");
+
+  if (!self->backlight) {
+    g_dbus_method_invocation_return_error (invocation,
+                                           G_DBUS_ERROR,
+                                           G_DBUS_ERROR_FILE_NOT_FOUND,
+                                           "No backlight");
+    return TRUE;
+  }
+
+  if (arg_enable) {
+    target = g_settings_get_int (self->settings_power, "idle-brightness") * 0.01;
+    self->saved_brightness = phosh_backlight_get_relative (self->backlight);
+  } else {
+    target = self->saved_brightness;
+    self->saved_brightness = -1.0;
+  }
+
+  if (target >= 0.0)
+    phosh_backlight_set_relative (self->backlight, target);
+
+  phosh_dbus_brightness_complete_set_dimming (object, invocation);
+  return TRUE;
+}
+
+
+static gboolean
+phosh_brightness_manager_handle_get_has_brightness_control (PhoshDBusBrightness *object)
+{
+  PhoshBrightnessManager *self = PHOSH_BRIGHTNESS_MANAGER (object);
+
+  return !!self->backlight;
+}
+
+
+static void
+phosh_brightness_manager_brightness_init (PhoshDBusBrightnessIface *iface)
+{
+  iface->handle_set_auto_brightness_target = phosh_brightness_manager_handle_set_auto_brightness_target;
+  iface->handle_set_dimming = phosh_brightness_manager_handle_set_dimming;
+  iface->get_has_brightness_control = phosh_brightness_manager_handle_get_has_brightness_control;
+}
 
 
 static void
@@ -87,6 +199,8 @@ set_backlight (PhoshBrightnessManager *self, PhoshBacklight *backlight)
     g_signal_handlers_disconnect_by_data (self->backlight, self);
 
   g_set_object (&self->backlight, backlight);
+  self->saved_brightness = -1.0;
+
   if (self->backlight) {
     g_debug ("Found %s for brightness control", phosh_backlight_get_name (self->backlight));
 
@@ -116,6 +230,9 @@ on_primary_monitor_changed (PhoshBrightnessManager *self, GParamSpec *psepc, Pho
   }
 
   set_backlight (self, backlight);
+
+  phosh_dbus_brightness_set_has_brightness_control (PHOSH_DBUS_BRIGHTNESS (self),
+                                                    !!self->backlight);
 }
 
 
@@ -225,9 +342,15 @@ phosh_brightness_manager_dispose (GObject *object)
 {
   PhoshBrightnessManager *self = PHOSH_BRIGHTNESS_MANAGER (object);
 
+  g_clear_handle_id (&self->dbus_name_id, g_bus_unown_name);
+
+  if (g_dbus_interface_skeleton_get_object_path (G_DBUS_INTERFACE_SKELETON (self)))
+    g_dbus_interface_skeleton_unexport (G_DBUS_INTERFACE_SKELETON (self));
+
   set_backlight (self, NULL);
   g_clear_pointer (&self->action_names, g_strfreev);
   g_clear_object (&self->settings);
+  g_clear_object (&self->settings_power);
   g_clear_signal_handler (&self->value_changed_id, self->adjustment);
   g_clear_object (&self->adjustment);
 
@@ -251,6 +374,8 @@ phosh_brightness_manager_init (PhoshBrightnessManager *self)
   GSettingsSchemaSource *source = g_settings_schema_source_get_default ();
   g_autoptr (GSettingsSchema) schema = NULL;
 
+  self->saved_brightness = -1.0;
+  self->settings_power = g_settings_new (POWER_SCHEMA);
   self->adjustment = g_object_ref_sink (gtk_adjustment_new (0, 0, 100, 10, 10, 0));
   self->value_changed_id = g_signal_connect_swapped (self->adjustment,
                                                      "value-changed",
@@ -279,6 +404,16 @@ phosh_brightness_manager_init (PhoshBrightnessManager *self)
                            self,
                            G_CONNECT_SWAPPED);
   on_primary_monitor_changed (self, NULL, shell);
+
+  self->dbus_name_id = g_bus_own_name (G_BUS_TYPE_SESSION,
+                                       "org.gnome.Shell.Brightness",
+                                       G_BUS_NAME_OWNER_FLAGS_ALLOW_REPLACEMENT |
+                                       G_BUS_NAME_OWNER_FLAGS_REPLACE,
+                                       on_bus_acquired,
+                                       on_name_acquired,
+                                       on_name_lost,
+                                       self,
+                                       NULL);
 }
 
 

src/brightness-manager.h
@@ -6,13 +6,16 @@
 
 #pragma once
 
+#include "phosh-brightness-dbus.h"
+
 #include <gtk/gtk.h>
 
 G_BEGIN_DECLS
 
 #define PHOSH_TYPE_BRIGHTNESS_MANAGER (phosh_brightness_manager_get_type ())
 
-G_DECLARE_FINAL_TYPE (PhoshBrightnessManager, phosh_brightness_manager, PHOSH, BRIGHTNESS_MANAGER, GObject)
+G_DECLARE_FINAL_TYPE (PhoshBrightnessManager, phosh_brightness_manager, PHOSH, BRIGHTNESS_MANAGER,
+                      PhoshDBusBrightnessSkeleton)
 
 PhoshBrightnessManager *phosh_brightness_manager_new (void);
 GtkAdjustment *         phosh_brightness_manager_get_adjustment (PhoshBrightnessManager *self);

src/dbus/meson.build
@@ -59,6 +59,12 @@ dbus_server_protos = [
   ],
   ['phosh-screen-saver-dbus', 'org.gnome.ScreenSaver.xml', 'org.gnome', false],
   [
+    'phosh-brightness-dbus',
+    'org.gnome.Shell.Brightness.xml',
+    'org.gnome.Shell',
+    false,
+  ],
+  [
     'phosh-screenshot-dbus',
     'org.gnome.Shell.Screenshot.xml',
     'org.gnome.Shell',

src/dbus/org.gnome.Shell.Brightness.xml
@@ -1,0 +1,45 @@
+<!DOCTYPE node PUBLIC
+'-//freedesktop//DTD D-BUS Object Introspection 1.0//EN'
+'http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd'>
+<!--
+  SPDX-License-Identifier: GPL-3.0-or-later
+-->
+<node>
+
+  <!--
+      org.gnome.Shell.Brightness:
+      @short_description: Brightness interface
+
+      The interface used to give the shell brightness related information.
+  -->
+  <interface name="org.gnome.Shell.Brightness">
+
+    <!--
+        SetDimming:
+        @enable: whether to enabled dimming
+
+        Enable or disable screen dimming to save power.
+    -->
+    <method name="SetDimming">
+      <arg type="b" direction="in" name="enable"/>
+    </method>
+
+    <!--
+        SetAutoBrightnessTarget:
+        @luminance: the target brightness
+
+        This lets the auto brightness system tell the shell what the ideal
+        relative brightness [0,1] is.
+    -->
+    <method name="SetAutoBrightnessTarget">
+      <arg type="d" direction="in" name="target"/>
+    </method>
+
+    <!--
+        HasBrightnessControl:
+        If any display has brightness control.
+    -->
+    <property name="HasBrightnessControl" type="b" access="read"/>
+
+  </interface>
+</node>

commit d7b65199a372964b0ab1c9f29e8ff4acc33dc660

brightness-manager: Don't raise brightness on idle
When idle dimming is enabled we never want to increase the brightness.
If current brightness is below target dimming brightness just don't do
anything.

Fixes: 57ee80353 ("brightness-manager: Add Brightness DBus interface")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1833>

src/brightness-manager.c
@@ -308,8 +308,17 @@ phosh_brightness_manager_handle_set_dimming (PhoshDBusBrightness   *object,
   }
 
   if (arg_enable) {
+    double current = phosh_backlight_get_relative (self->backlight);
+
     target = g_settings_get_int (self->settings_power, "idle-brightness") * 0.01;
-    self->saved_brightness = phosh_backlight_get_relative (self->backlight);
+
+    /* If current brightness is lower than dim brightness don't do anything */
+    if (target >= current) {
+      self->saved_brightness = -1.0;
+      goto done;
+    }
+
+    self->saved_brightness = current;
   } else {
     target = self->saved_brightness;
     self->saved_brightness = -1.0;
@@ -318,6 +327,7 @@ phosh_brightness_manager_handle_set_dimming (PhoshDBusBrightness   *object,
   if (target >= 0.0)
     phosh_backlight_set_relative (self->backlight, target);
 
+ done:
   phosh_dbus_brightness_complete_set_dimming (object, invocation);
   return TRUE;
 }

tests/integration/test_dbus.py
@@ -38,17 +38,17 @@ class PhoshDBusTestCase(DBusTestCase):
         klass.umock = UMockdev.Testbed.new()
 
         # Torch
-        syspath = klass.umock.add_device(
+        klass.torch_syspath = klass.umock.add_device(
             "leds",
             "white:flash",
             None,
             ["brightness", "0", "max_brightness", "255"],
             ["GM_TORCH_MIN_BRIGHTNESS", "1"],
         )
-        assert syspath == "/sys/devices/white:flash"
+        assert klass.torch_syspath == "/sys/devices/white:flash"
 
         # Backlight
-        syspath = klass.umock.add_device(
+        klass.backlight_syspath = klass.umock.add_device(
             "backlight",
             "intel_backlight",
             None,
@@ -66,7 +66,7 @@ class PhoshDBusTestCase(DBusTestCase):
             ],
             [],
         )
-        assert syspath == "/sys/devices/intel_backlight"
+        assert klass.backlight_syspath == "/sys/devices/intel_backlight"
 
         return klass.umock.get_root_dir()
 