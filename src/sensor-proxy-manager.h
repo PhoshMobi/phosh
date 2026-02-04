8e334b541f39e3bd4134ecc0e0d7dbb6971a31b6
Evangelos Ribeiro Tzaras <devrtz@fortysixandtwo.eu>
2026-02-04 10:52:30 +0100

    sensor-proxy-manager: Remove non-existing function from headers
    Signed-off-by: Evangelos Ribeiro Tzaras <devrtz@fortysixandtwo.eu>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1871>

--- src/sensor-proxy-manager.h
+++ src/sensor-proxy-manager.h
@@ -14,5 +14,3 @@ G_DECLARE_FINAL_TYPE (PhoshSensorProxyManager, phosh_sensor_proxy_manager,
                       PHOSH, SENSOR_PROXY_MANAGER, PhoshDBusSensorProxyProxy)
 
 PhoshSensorProxyManager *phosh_sensor_proxy_manager_new (GError **err);
-gboolean phosh_sensor_proxy_manager_claim_proximity_sync (PhoshSensorProxyManager *self,
-                                                          GError **err);
