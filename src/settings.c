PhoshMobi/phosh

commit e90c7e33be62e06bf9858bb5250e710a565f6135

top-panel: Move lauching settings here
This allows us to use it from all places in the top panel

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1768>

src/settings.c
@@ -206,59 +206,12 @@ update_drag_handle_offset (PhoshSettings *self)
 
 
 static void
-close_settings_menu (PhoshSettings *self)
-{
-  g_signal_emit (self, signals[SETTING_DONE], 0);
-}
-
-static void
 brightness_value_changed_cb (GtkScale *scale_brightness, gpointer unused)
 {
   int brightness;
 
   brightness = (int)gtk_range_get_value (GTK_RANGE (scale_brightness));
   brightness_set (brightness);
-}
-
-
-static void
-open_settings_panel (PhoshSettings *self, gboolean mobile, const char *panel)
-{
-  if (self->on_lockscreen)
-    return;
-
-  if (mobile)
-    phosh_util_open_mobile_settings_panel (panel);
-  else
-    phosh_util_open_settings_panel (panel);
-
-  close_settings_menu (self);
-}
-
-
-static void
-on_launch_panel_activated (GSimpleAction *action, GVariant *param, gpointer data)
-{
-  PhoshSettings *self = PHOSH_SETTINGS (data);
-  const char *panel;
-
-  panel = g_variant_get_string (param, NULL);
-
-  open_settings_panel (self, FALSE, panel);
-  phosh_settings_hide_details (self);
-}
-
-
-static void
-on_launch_mobile_panel_activated (GSimpleAction *action, GVariant *param, gpointer data)
-{
-  PhoshSettings *self = PHOSH_SETTINGS (data);
-  const char *panel;
-
-  panel = g_variant_get_string (param, NULL);
-
-  open_settings_panel (self, TRUE, panel);
-  phosh_settings_hide_details (self);
 }
 
 
@@ -523,10 +476,6 @@ phosh_settings_class_init (PhoshSettingsClass *klass)
 
   g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
 
-  signals[SETTING_DONE] = g_signal_new ("setting-done",
-                                        G_TYPE_FROM_CLASS (klass), G_SIGNAL_RUN_LAST, 0, NULL, NULL,
-                                        NULL, G_TYPE_NONE, 0);
-
   g_type_ensure (PHOSH_TYPE_AUDIO_SETTINGS);
   g_type_ensure (PHOSH_TYPE_QUICK_SETTINGS);
 
@@ -551,40 +500,10 @@ phosh_settings_class_init (PhoshSettingsClass *klass)
 }
 
 
-static const GActionEntry entries[] = {
-  { .name = "launch-panel", .activate = on_launch_panel_activated, .parameter_type = "s" },
-  { .name = "launch-mobile-panel",
-    .activate = on_launch_mobile_panel_activated,
-    .parameter_type = "s" },
-};
-
-
 static void
 phosh_settings_init (PhoshSettings *self)
 {
-  g_autoptr (GActionMap) map = NULL;
-  GAction *action;
-
   gtk_widget_init_template (GTK_WIDGET (self));
-
-  map = G_ACTION_MAP (g_simple_action_group_new ());
-  g_action_map_add_action_entries (map,
-                                   entries,
-                                   G_N_ELEMENTS (entries),
-                                   self);
-  gtk_widget_insert_action_group (GTK_WIDGET (self),
-                                  "settings",
-                                  G_ACTION_GROUP (map));
-
-  action = g_action_map_lookup_action (map, "launch-panel");
-  g_object_bind_property (self, "on-lockscreen",
-                          action, "enabled",
-                          G_BINDING_SYNC_CREATE | G_BINDING_INVERT_BOOLEAN);
-
-  action = g_action_map_lookup_action (map, "launch-mobile-panel");
-  g_object_bind_property (self, "on-lockscreen",
-                          action, "enabled",
-                          G_BINDING_SYNC_CREATE | G_BINDING_INVERT_BOOLEAN);
 
   g_signal_connect (self, "size-allocate", G_CALLBACK (on_size_allocate), NULL);
 }

src/ui/top-panel.ui
@@ -198,7 +198,6 @@
           <object class="PhoshSettings" id="settings">
             <property name="visible">1</property>
             <signal name="notify::drag-handle-offset" handler="on_settings_drag_handle_offset_changed" swapped="true"/>
-            <signal name="setting-done" handler="phosh_top_panel_fold" swapped="true"/>
           </object>
         </child>
         <child>

src/quick-setting.c
@@ -48,7 +48,7 @@
  * When a quick-setting is clicked, [signal@Phosh.QuickSetting::clicked] is emitted. When it is
  * long-pressed or right-clicked, [signal@PhoshQuickSetting::long-pressed] is emitted.
  *
- * The common usecase of `long-pressed` is to launch an action (like `settings.launch-panel`). So to
+ * The common usecase of `long-pressed` is to launch an action (like `panel.launch-panel`). So to
  * avoid duplicating this process for each quick-setting, the user can set
  * [property@Phosh.QuickSetting:long-press-action-name] and
  * [property@Phosh.QuickSetting:long-press-action-target]. The quick-setting then launches that

src/top-panel.c
@@ -253,6 +253,47 @@ on_logout_action (GSimpleAction *action,
 
 
 static void
+open_settings_panel (PhoshTopPanel *self, gboolean mobile, const char *panel)
+{
+  if (self->on_lockscreen)
+    return;
+
+  if (mobile)
+    phosh_util_open_mobile_settings_panel (panel);
+  else
+    phosh_util_open_settings_panel (panel);
+
+  phosh_top_panel_fold (self);
+}
+
+
+static void
+on_launch_panel_activated (GSimpleAction *action, GVariant *param, gpointer data)
+{
+  PhoshTopPanel *self = PHOSH_TOP_PANEL (data);
+  const char *panel;
+
+  panel = g_variant_get_string (param, NULL);
+
+  open_settings_panel (self, FALSE, panel);
+  phosh_settings_hide_details (PHOSH_SETTINGS (self->settings));
+}
+
+
+static void
+on_launch_mobile_panel_activated (GSimpleAction *action, GVariant *param, gpointer data)
+{
+  PhoshTopPanel *self = PHOSH_TOP_PANEL (data);
+  const char *panel;
+
+  panel = g_variant_get_string (param, NULL);
+
+  open_settings_panel (self, TRUE, panel);
+  phosh_settings_hide_details (PHOSH_SETTINGS (self->settings));
+}
+
+
+static void
 wall_clock_notify_cb (PhoshTopPanel  *self,
                       GParamSpec     *pspec,
                       PhoshWallClock *wall_clock)
@@ -554,6 +595,10 @@ static GActionEntry entries[] = {
   { .name = "suspend", .activate = on_suspend_action },
   { .name = "lockscreen", .activate = on_lockscreen_action },
   { .name = "logout", .activate = on_logout_action },
+  { .name = "launch-panel", .activate = on_launch_panel_activated, .parameter_type = "s" },
+  { .name = "launch-mobile-panel",
+    .activate = on_launch_mobile_panel_activated,
+    .parameter_type = "s" },
 };
 
 
@@ -564,6 +609,7 @@ phosh_top_panel_constructed (GObject *object)
   GdkDisplay *display = gdk_display_get_default ();
   PhoshWallClock *wall_clock = phosh_wall_clock_get_default ();
   PhoshShell *shell = phosh_shell_get_default ();
+  GAction *action;
 
   g_autoptr (GSettings) phosh_settings = g_settings_new ("sm.puri.phosh");
 
@@ -620,7 +666,7 @@ phosh_top_panel_constructed (GObject *object)
                                    entries, G_N_ELEMENTS (entries),
                                    self);
   if (!phosh_shell_started_by_display_manager (phosh_shell_get_default ())) {
-    GAction *action = g_action_map_lookup_action (G_ACTION_MAP (self->actions), "logout");
+    action = g_action_map_lookup_action (G_ACTION_MAP (self->actions), "logout");
     g_simple_action_set_enabled (G_SIMPLE_ACTION (action), FALSE);
   }
 
@@ -629,6 +675,16 @@ phosh_top_panel_constructed (GObject *object)
                    g_action_map_lookup_action (G_ACTION_MAP (self->actions), "suspend"),
                    "enabled",
                    G_SETTINGS_BIND_GET);
+
+  action = g_action_map_lookup_action (G_ACTION_MAP (self->actions), "launch-panel");
+  g_object_bind_property (self, "on-lockscreen",
+                          action, "enabled",
+                          G_BINDING_SYNC_CREATE | G_BINDING_INVERT_BOOLEAN);
+
+  action = g_action_map_lookup_action (G_ACTION_MAP (self->actions), "launch-mobile-panel");
+  g_object_bind_property (self, "on-lockscreen",
+                          action, "enabled",
+                          G_BINDING_SYNC_CREATE | G_BINDING_INVERT_BOOLEAN);
 
 
   self->interface_settings = g_settings_new ("org.gnome.desktop.interface");

src/ui/audio-settings.ui
@@ -128,7 +128,7 @@
                 <child>
                   <object class="GtkButton">
                     <property name="visible">1</property>
-                    <property name="action-name">settings.launch-panel</property>
+                    <property name="action-name">panel.launch-panel</property>
                     <property name="action-target">&quot;sound&quot;</property>
                     <child>
                       <object class="GtkLabel">

src/ui/bt-status-page.ui
@@ -44,7 +44,7 @@
   <object class="GtkButton" id="footer">
     <property name="visible">1</property>
     <property name="hexpand">1</property>
-    <property name="action-name">settings.launch-panel</property>
+    <property name="action-name">panel.launch-panel</property>
     <property name="action-target">&quot;bluetooth&quot;</property>
     <property name="focus-on-click">0</property>
     <child>

src/ui/feedback-status-page.ui
@@ -42,7 +42,7 @@
   <object class="GtkButton" id="footer">
     <property name="visible">1</property>
     <property name="hexpand">1</property>
-    <property name="action-name">settings.launch-mobile-panel</property>
+    <property name="action-name">panel.launch-mobile-panel</property>
     <property name="action-target">&quot;feedback&quot;</property>
     <property name="focus-on-click">0</property>
     <child>

src/ui/quick-settings.ui
@@ -12,7 +12,7 @@
             <property name="visible">1</property>
             <property name="sensitive" bind-source="wwaninfo" bind-property="present" bind-flags="sync-create"/>
             <property name="status-icon">wwaninfo</property>
-            <property name="long-press-action-name">settings.launch-panel</property>
+            <property name="long-press-action-name">panel.launch-panel</property>
             <property name="long-press-action-target">&quot;wwan&quot;</property>
             <signal name="clicked" handler="on_wwan_clicked" object="PhoshQuickSettings" swapped="yes"/>
           </object>
@@ -23,7 +23,7 @@
             <property name="sensitive" bind-source="wifiinfo" bind-property="present" bind-flags="sync-create"/>
             <property name="status-icon">wifiinfo</property>
             <property name="status-page">wifi_status_page</property>
-            <property name="long-press-action-name">settings.launch-panel</property>
+            <property name="long-press-action-name">panel.launch-panel</property>
             <property name="long-press-action-target">&quot;wifi&quot;</property>
             <signal name="clicked" handler="on_wifi_clicked" object="PhoshQuickSettings" swapped="yes"/>
           </object>
@@ -34,7 +34,7 @@
             <property name="sensitive" bind-source="btinfo" bind-property="present" bind-flags="sync-create"/>
             <property name="status-icon">btinfo</property>
             <property name="status-page">bt_status_page</property>
-            <property name="long-press-action-name">settings.launch-panel</property>
+            <property name="long-press-action-name">panel.launch-panel</property>
             <property name="long-press-action-target">&quot;bluetooth&quot;</property>
             <signal name="clicked" handler="on_bt_clicked" object="PhoshQuickSettings" swapped="yes"/>
           </object>
@@ -44,7 +44,7 @@
             <property name="visible">1</property>
             <property name="sensitive" bind-source="batteryinfo" bind-property="present" bind-flags="sync-create"/>
             <property name="status-icon">batteryinfo</property>
-            <property name="long-press-action-name">settings.launch-panel</property>
+            <property name="long-press-action-name">panel.launch-panel</property>
             <property name="long-press-action-target">&quot;power&quot;</property>
             <signal name="clicked" handler="on_battery_clicked" object="PhoshQuickSettings" swapped="yes"/>
           </object>
@@ -63,7 +63,7 @@
             <property name="visible" bind-source="feedbackinfo" bind-property="present" bind-flags="sync-create"/>
             <property name="status-icon">feedbackinfo</property>
             <property name="status-page">feedback_status_page</property>
-            <property name="long-press-action-name">settings.launch-panel</property>
+            <property name="long-press-action-name">panel.launch-panel</property>
             <property name="long-press-action-target">&quot;notifications&quot;</property>
             <signal name="clicked" handler="on_feedback_clicked" object="PhoshQuickSettings" swapped="yes"/>
           </object>
@@ -72,7 +72,7 @@
           <object class="PhoshQuickSetting">
             <property name="visible" bind-source="torchinfo" bind-property="present" bind-flags="sync-create"/>
             <property name="status-icon">torchinfo</property>
-            <property name="long-press-action-name">settings.launch-panel</property>
+            <property name="long-press-action-name">panel.launch-panel</property>
             <property name="long-press-action-target">&quot;power&quot;</property>
             <signal name="clicked" handler="on_torch_clicked" object="PhoshQuickSettings" swapped="yes"/>
           </object>
@@ -81,7 +81,7 @@
           <object class="PhoshQuickSetting">
             <property name="visible" bind-source="dockedinfo" bind-property="present" bind-flags="sync-create"/>
             <property name="status-icon">dockedinfo</property>
-            <property name="long-press-action-name">settings.launch-panel</property>
+            <property name="long-press-action-name">panel.launch-panel</property>
             <property name="long-press-action-target">&quot;display&quot;</property>
             <signal name="clicked" handler="on_docked_clicked" object="PhoshQuickSettings" swapped="yes"/>
           </object>
@@ -90,7 +90,7 @@
           <object class="PhoshQuickSetting">
             <property name="visible" bind-source="vpninfo" bind-property="present" bind-flags="sync-create"/>
             <property name="status-icon">vpninfo</property>
-            <property name="long-press-action-name">settings.launch-panel</property>
+            <property name="long-press-action-name">panel.launch-panel</property>
             <property name="long-press-action-target">&quot;network&quot;</property>
             <signal name="clicked" handler="on_vpn_clicked" object="PhoshQuickSettings" swapped="yes"/>
           </object>

commit 5b3214ccb437e10fe4d47d0a273a74fe39cdb7ee

setting: Close top panel when activating media player
This was accidentally dropped when moving the actions to the
top-panel. With GTK4 we can make the panel closing an action
too by using `gtk_widget_activate_action()`.

Fixes: e90c7e33b ("top-panel: Move lauching settings here")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1901>

src/settings.c
@@ -441,6 +441,13 @@ phosh_settings_class_init (PhoshSettingsClass *klass)
 
   g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
 
+  signals[SETTING_DONE] = g_signal_new ("setting-done",
+                                        G_TYPE_FROM_CLASS (klass),
+                                        G_SIGNAL_RUN_LAST,
+                                        0, NULL, NULL, NULL,
+                                        G_TYPE_NONE,
+                                        0);
+
   g_type_ensure (PHOSH_TYPE_AUDIO_SETTINGS);
   g_type_ensure (PHOSH_TYPE_BRIGHTNESS_SETTINGS);
   g_type_ensure (PHOSH_TYPE_QUICK_SETTINGS);

src/ui/top-panel.ui
@@ -198,6 +198,7 @@
           <object class="PhoshSettings" id="settings">
             <property name="visible">1</property>
             <signal name="notify::drag-handle-offset" handler="on_settings_drag_handle_offset_changed" swapped="true"/>
+            <signal name="setting-done" handler="phosh_top_panel_fold" swapped="true"/>
           </object>
         </child>
         <child>