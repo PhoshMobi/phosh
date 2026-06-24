PhoshMobi/phosh

commit 049fe7cf65407b4ab55f36acd00ca5bbde9e91b3

backlight: Add and use methods to get and set relative brightness
No need to make the calculations in multiple places

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1785>

src/backlight.c
@@ -252,6 +252,43 @@ phosh_backlight_set_brightness (PhoshBacklight *self, int brightness)
   }
 }
 
+/**
+ * phosh_backlight_set_relative:
+ * @self: The backlight
+ * @val: The relative brightness
+ *
+ * The relative brightness value between `[0.0, 1.0]` is applied
+ * linearly between the backlight's min and max brightness.
+ */
+void
+phosh_backlight_set_relative (PhoshBacklight *self, double val)
+{
+  PhoshBacklightPrivate *priv = phosh_backlight_get_instance_private (self);
+  int brightness;
+
+  g_return_if_fail (PHOSH_IS_BACKLIGHT (self));
+  g_return_if_fail (val >= 0.0 && val <= 1.0);
+
+  brightness = priv->min_brightness + ((priv->max_brightness - priv->min_brightness) * val);
+  phosh_backlight_set_brightness (self, brightness);
+}
+
+/**
+ * phosh_backlight_get_relative:
+ * @self: The backlight
+ * @val: The relative brightness
+ *
+ * Get the relative brightness value between `[0.0, 1.0]`.
+ */
+double
+phosh_backlight_get_relative (PhoshBacklight *self)
+{
+  PhoshBacklightPrivate *priv = phosh_backlight_get_instance_private (self);
+
+  return 1.0 * (priv->target_brightness - priv->min_brightness) /
+    (priv->max_brightness - priv->min_brightness);
+}
+
 
 const char *
 phosh_backlight_get_name (PhoshBacklight *self)

src/backlight.h
@@ -16,6 +16,8 @@ G_DECLARE_DERIVABLE_TYPE (PhoshBacklight, phosh_backlight, PHOSH, BACKLIGHT, GOb
 
 int          phosh_backlight_get_brightness (PhoshBacklight *self);
 void         phosh_backlight_set_brightness (PhoshBacklight *self, int brightness);
+double       phosh_backlight_get_relative (PhoshBacklight *self);
+void         phosh_backlight_set_relative (PhoshBacklight *self, double val);
 void         phosh_backlight_get_range (PhoshBacklight *self,
                                         int            *min_brightness,
                                         int            *max_brightness);

src/brightness-manager.c
@@ -84,7 +84,7 @@ adjust_brightness (PhoshBrightnessManager *self, gboolean up)
   if (phosh_shell_get_state (shell) & PHOSH_STATE_SETTINGS)
     return;
 
-  percentage = 100.0 * (brightness - min) / (max - min);
+  percentage = 100.0 * phosh_backlight_get_relative (self->backlight);
   phosh_shell_show_osd (phosh_shell_get_default (),
                         NULL,
                         "display-brightness-symbolic",

src/settings/brightness.c
@@ -28,7 +28,6 @@ on_brightness_changed (PhoshBacklight *backlight_,
                        gpointer        user_data)
 {
   GtkScale *scale = GTK_SCALE (user_data);
-  int brightness, min = 0, max = 0;
   double value;
 
   g_assert (backlight == backlight_);
@@ -36,10 +35,7 @@ on_brightness_changed (PhoshBacklight *backlight_,
   if (setting_brightness)
     return;
 
-  brightness = phosh_backlight_get_brightness (backlight);
-  phosh_backlight_get_range (backlight, &min, &max);
-
-  value = 100.0 * (brightness - min) / (max - min);
+  value = 100.0 * phosh_backlight_get_relative (backlight);
 
   g_signal_handler_block (G_OBJECT (scale), scale_handler_id);
   gtk_range_set_value (GTK_RANGE (scale), value);
@@ -96,8 +92,6 @@ brightness_init (GtkScale *scale, gulong handler_id)
 void
 brightness_set (int value)
 {
-  int brightness, min = 0, max = 0;
-
   if (!backlight)
     return;
 
@@ -106,10 +100,7 @@ brightness_set (int value)
 
   setting_brightness = TRUE;
 
-  phosh_backlight_get_range (backlight, &min, &max);
-
-  brightness = min + ((max - min) * (value * 0.01));
-  phosh_backlight_set_brightness (backlight, brightness);
+  phosh_backlight_set_relative (backlight, value * 0.01);
   setting_brightness = FALSE;
 }
 

commit 8aaec658842e2a21eb6e2065c3f44a9789f86105

backlight: Guard public method
Follow our usual pattern.

Fixes: 049fe7cf6 ("backlight: Add and use methods to get and set relative brightness")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1912>

src/backlight.c
@@ -368,6 +368,8 @@ phosh_backlight_get_relative (PhoshBacklight *self)
 {
   PhoshBacklightPrivate *priv = phosh_backlight_get_instance_private (self);
 
+  g_return_val_if_fail (PHOSH_IS_BACKLIGHT (self), 1.0);
+
   return 1.0 * (priv->brightness.target - priv->brightness.min) /
     (priv->brightness.max - priv->brightness.min);
 }