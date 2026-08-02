PhoshMobi/phosh

commit 74e88ef6c1513e921edbd53ef65813317f23f385

caffeine: Allow to select caffeine duration
With this, we add support for custom intervals
in caffeine. It will decide how long caffeine
will stay on for. Intervals can be added/selected
individually via gsettings keys.

Signed-off-by: Rudra Pratap Singh <rudrasingh900@gmail.com>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1766>

plugins/caffeine-quick-setting/caffeine-quick-setting.c
@@ -7,10 +7,18 @@
  */
 
 #include "caffeine-quick-setting.h"
+#include "interval-row.h"
 #include "plugin-shell.h"
+
+#include <cui-call.h>
 
 #include <glib/gi18n.h>
 
+#define CAFFEINE_QUICK_SETTING_SCHEMA "mobi.phosh.plugins.caffeine-quick-setting"
+#define CAFFEINE_INTERVALS_KEY        "intervals"
+#define CAFFEINE_SELECTED_KEY         "selected-index"
+
+#define UPDATE_INTERVAL    1 /* seconds */
 #define CAFFEINE_ON_ICON   "cafe-hot-symbolic"
 #define CAFFEINE_OFF_ICON  "cafe-cold-symbolic"
 
@@ -32,10 +40,17 @@ struct _PhoshCaffeineQuickSetting {
 
   PhoshStatusIcon         *info;
   guint                    cookie;
+
+  GtkStack                *stack;
+  GtkListBox              *listbox;
+  GtkListBoxRow           *cur_row;
+  GSettings               *settings;
+
+  uint                     remaining;
+  uint                     update_id;
 };
 
 G_DEFINE_TYPE (PhoshCaffeineQuickSetting, phosh_caffeine_quick_setting, PHOSH_TYPE_QUICK_SETTING);
-
 
 static void
 phosh_caffeine_quick_setting_inhibit (PhoshCaffeineQuickSetting *self, gboolean inhibit)
@@ -99,9 +114,87 @@ phosh_caffeine_quick_setting_get_property (GObject *object,
 
 
 static void
+phosh_caffeine_quick_setting_clear_timer (PhoshCaffeineQuickSetting *self)
+{
+  self->remaining = 0;
+
+  g_clear_handle_id (&self->update_id, g_source_remove);
+
+  phosh_caffeine_quick_setting_inhibit (self, FALSE);
+}
+
+
+static gboolean
+on_update_expired (gpointer user_data)
+{
+  PhoshCaffeineQuickSetting *self = PHOSH_CAFFEINE_QUICK_SETTING (user_data);
+
+  g_return_val_if_fail (self->remaining >= UPDATE_INTERVAL, FALSE);
+  self->remaining -= UPDATE_INTERVAL;
+  if (self->remaining > 0) {
+    g_autofree char *label = cui_call_format_duration ((double) self->remaining);
+    phosh_status_icon_set_info (self->info, label);
+
+    return G_SOURCE_CONTINUE;
+  }
+
+  phosh_caffeine_quick_setting_clear_timer (self);
+
+  return G_SOURCE_REMOVE;
+}
+
+
+static void
 on_clicked (PhoshCaffeineQuickSetting *self)
 {
-  phosh_caffeine_quick_setting_inhibit (self, !self->cookie);
+  uint value;
+
+  if (self->cur_row)
+    value = phosh_interval_row_get_value (PHOSH_INTERVAL_ROW (self->cur_row));
+  else
+    value = PHOSH_INTERVAL_ROW_INFINITY_VALUE; /* No interval, default to infinity */
+
+  if (value >= PHOSH_INTERVAL_ROW_INFINITY_VALUE) {
+    phosh_caffeine_quick_setting_inhibit (self, !self->cookie);
+
+    return;
+  }
+
+  /* Clear timer if on */
+  if (self->update_id) {
+    phosh_caffeine_quick_setting_clear_timer (self);
+
+    return;
+  }
+
+  self->remaining = value;
+  self->update_id = g_timeout_add_seconds (UPDATE_INTERVAL, on_update_expired, self);
+  phosh_caffeine_quick_setting_inhibit (self, TRUE);
+}
+
+
+static void
+on_interval_row_activated (GtkListBox                *listbox,
+                           GtkListBoxRow             *row,
+                           PhoshCaffeineQuickSetting *self)
+{
+  uint selected_idx = 0;
+  g_autoptr (GList) children = NULL;
+
+  if (self->cur_row == row)
+    return;
+
+  phosh_caffeine_quick_setting_clear_timer (self);
+
+  children = gtk_container_get_children (GTK_CONTAINER (self->listbox));
+  for (GList *child = children; child; child = child->next) {
+    if (child->data == row)
+      break;
+
+    selected_idx++;
+  }
+
+  g_settings_set_uint (self->settings, CAFFEINE_SELECTED_KEY, selected_idx);
 }
 
 
@@ -126,11 +219,18 @@ transform_to_label (GBinding     *binding,
                     GValue       *to_value,
                     gpointer      user_data)
 {
+  PhoshCaffeineQuickSetting *self = PHOSH_CAFFEINE_QUICK_SETTING (user_data);
   gboolean inhibited = g_value_get_boolean (from_value);
-  const char *label;
 
-  label = inhibited ? C_("caffeine-enabled", "On") : C_("caffeine-disabled", "Off");
-  g_value_set_string (to_value, label);
+  if (!inhibited) {
+    g_value_set_string (to_value, C_("caffeine-disabled", "Off"));
+  } else if (self->update_id) {
+    g_autofree char *label = cui_call_format_duration ((uint) self->remaining);
+    g_value_set_string (to_value, label);
+  } else {
+    g_value_set_string (to_value, C_("caffeine-enabled", "On"));
+  }
+
   return TRUE;
 }
 
@@ -142,6 +242,8 @@ phosh_caffeine_quick_setting_finalize (GObject *gobject)
 
   if (self->cookie)
     phosh_caffeine_quick_setting_inhibit (self, FALSE);
+
+  g_clear_object (&self->settings);
 
   G_OBJECT_CLASS (phosh_caffeine_quick_setting_parent_class)->finalize (gobject);
 }
@@ -168,18 +270,107 @@ phosh_caffeine_quick_setting_class_init (PhoshCaffeineQuickSettingClass *klass)
                                                "/mobi/phosh/plugins/caffeine-quick-setting/qs.ui");
 
   gtk_widget_class_bind_template_child (widget_class, PhoshCaffeineQuickSetting, info);
+  gtk_widget_class_bind_template_child (widget_class, PhoshCaffeineQuickSetting, listbox);
+  gtk_widget_class_bind_template_child (widget_class, PhoshCaffeineQuickSetting, stack);
 
   gtk_widget_class_bind_template_callback (widget_class, on_clicked);
+  gtk_widget_class_bind_template_callback (widget_class, on_interval_row_activated);
+}
+
+
+static int
+sort_rows_func (GtkListBoxRow *r1, GtkListBoxRow *r2, gpointer user_data)
+{
+  uint v1 = phosh_interval_row_get_value (PHOSH_INTERVAL_ROW (r1));
+  uint v2 = phosh_interval_row_get_value (PHOSH_INTERVAL_ROW (r2));
+
+  /* Overflow is possible as values are unsigned,
+   * so we compare them manually. */
+  if (v1 == v2)
+    return 0;
+
+  return v1 < v2 ? -1 : 1;
 }
 
 
 static void
-phosh_caffeine_quick_setting_init (PhoshCaffeineQuickSetting *self)
+on_selected_index_changed (PhoshCaffeineQuickSetting *self)
 {
+  uint selected_idx = g_settings_get_uint (self->settings, CAFFEINE_SELECTED_KEY);
+  g_autoptr (GList) children = gtk_container_get_children (GTK_CONTAINER (self->listbox));
+  uint len = g_list_length (children);
+
+  phosh_caffeine_quick_setting_clear_timer (self);
+
+  /* Possible that we don't have any intervals */
+  if (len) {
+    /* If index is out of bounds, select the last in the list */
+    if (selected_idx >= len) {
+      g_debug ("Invalid interval index, defaulting to last interval");
+
+      selected_idx = len - 1;
+    }
+
+    if (self->cur_row)
+      phosh_interval_row_set_selected (PHOSH_INTERVAL_ROW (self->cur_row), FALSE);
+
+    self->cur_row = gtk_list_box_get_row_at_index (self->listbox, selected_idx);
+    phosh_interval_row_set_selected (PHOSH_INTERVAL_ROW (self->cur_row), TRUE);
+  }
+}
+
+
+static void
+on_intervals_changed (PhoshCaffeineQuickSetting *self)
+{
+  uint interval;
+  GVariantIter iter;
+  PhoshIntervalRow* row = NULL;
+  g_autoptr (GVariant) intervals = NULL;
+  g_autoptr (GList) children = gtk_container_get_children (GTK_CONTAINER (self->listbox));
+
+  g_debug ("Intervals changed, reconfiguring listbox...");
+
+  self->cur_row = NULL;
+
+  for (GList *child = children; child; child = child->next)
+    gtk_container_remove (GTK_CONTAINER (self->listbox), child->data);
+
+  intervals = g_settings_get_value (self->settings, CAFFEINE_INTERVALS_KEY);
+
+  g_variant_iter_init (&iter, intervals);
+  while (g_variant_iter_next (&iter, "u", &interval)) {
+    row = phosh_interval_row_new (interval, FALSE);
+    gtk_list_box_insert (self->listbox, GTK_WIDGET (row), -1);
+  }
+
+  on_selected_index_changed (self);
+  gtk_stack_set_visible_child_name (self->stack,
+                                    g_variant_n_children (intervals) ? "listbox" : "empty-state");
+}
+
+
+static void
+phosh_caffeine_quick_setting_init (PhoshCaffeineQuickSetting *self) {
   gtk_widget_init_template (GTK_WIDGET (self));
 
   gtk_icon_theme_add_resource_path (gtk_icon_theme_get_default (),
                                     "/mobi/phosh/plugins/caffeine-quick-setting/icons");
+
+  self->settings = g_settings_new (CAFFEINE_QUICK_SETTING_SCHEMA);
+
+  gtk_list_box_set_sort_func (self->listbox, sort_rows_func, NULL, NULL);
+
+  g_signal_connect_object (self->settings,
+                           "changed::" CAFFEINE_INTERVALS_KEY,
+                           G_CALLBACK (on_intervals_changed),
+                           self,
+                           G_CONNECT_SWAPPED);
+  g_signal_connect_object (self->settings,
+                           "changed::" CAFFEINE_SELECTED_KEY,
+                           G_CALLBACK (on_selected_index_changed),
+                           self,
+                           G_CONNECT_SWAPPED);
 
   g_object_bind_property (self, "inhibited",
                           self, "active",
@@ -196,5 +387,7 @@ phosh_caffeine_quick_setting_init (PhoshCaffeineQuickSetting *self)
                                self->info, "info",
                                G_BINDING_DEFAULT | G_BINDING_SYNC_CREATE,
                                transform_to_label,
-                               NULL, NULL, NULL);
+                               NULL, self, NULL);
+
+  on_intervals_changed (self);
 }

plugins/caffeine-quick-setting/interval-row.c
@@ -1,0 +1,174 @@
+/*
+ * Copyright (C) 2025 The Phosh Developers
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Rudra Pratap Singh <rudrasingh900@gmail.com>
+ */
+
+#include "interval-row.h"
+
+#include <cui-call.h>
+
+#include <glib/gi18n.h>
+
+/**
+ * PhoshIntervalRow:
+ *
+ * A widget to display timed intervals
+ */
+enum {
+  PROP_0,
+  PROP_VALUE,
+  PROP_SELECTED,
+  PROP_LAST_PROP,
+};
+static GParamSpec *props[PROP_LAST_PROP];
+
+struct _PhoshIntervalRow {
+  HdyActionRow  parent;
+
+  GtkRevealer  *revealer;
+  uint          value;
+  gboolean      selected;
+};
+
+G_DEFINE_TYPE (PhoshIntervalRow, phosh_interval_row, HDY_TYPE_ACTION_ROW);
+
+static void
+phosh_interval_row_set_value (PhoshIntervalRow *self, uint value)
+{
+  self->value = value;
+
+  if (value < PHOSH_INTERVAL_ROW_INFINITY_VALUE) {
+    g_autofree char *label = cui_call_format_duration ((double) value);
+
+    hdy_preferences_row_set_title (HDY_PREFERENCES_ROW (self), label);
+  } else {
+    hdy_preferences_row_set_title (HDY_PREFERENCES_ROW (self), "∞");
+  }
+
+  g_object_bind_property (self,
+                          "selected",
+                          self->revealer,
+                          "reveal-child",
+                          G_BINDING_DEFAULT | G_BINDING_SYNC_CREATE);
+}
+
+
+static void
+phosh_interval_row_set_property (GObject      *object,
+                                 guint         property_id,
+                                 const GValue *value,
+                                 GParamSpec   *pspec)
+{
+  PhoshIntervalRow *self = PHOSH_INTERVAL_ROW (object);
+
+  switch (property_id) {
+  case PROP_VALUE:
+    phosh_interval_row_set_value (self, g_value_get_uint (value));
+    break;
+  case PROP_SELECTED:
+    phosh_interval_row_set_selected (self, g_value_get_boolean (value));
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+  }
+}
+
+
+static void
+phosh_interval_row_get_property (GObject    *object,
+                                 guint       property_id,
+                                 GValue     *value,
+                                 GParamSpec *pspec)
+{
+  PhoshIntervalRow *self = PHOSH_INTERVAL_ROW (object);
+
+  switch (property_id) {
+  case PROP_SELECTED:
+    g_value_set_boolean (value, self->selected);
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    break;
+  }
+}
+
+
+static void
+phosh_interval_row_class_init (PhoshIntervalRowClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  object_class->get_property = phosh_interval_row_get_property;
+  object_class->set_property = phosh_interval_row_set_property;
+
+  /**
+   * PhoshIntervalRow:value:
+   *
+   * The value of interval, in seconds
+   */
+  props[PROP_VALUE] =
+    g_param_spec_uint ("value", "", "",
+                       0, G_MAXUINT, 0,
+                       G_PARAM_WRITABLE | G_PARAM_CONSTRUCT_ONLY | G_PARAM_STATIC_STRINGS);
+  /**
+   * PhoshIntervalRow:selected:
+   *
+   * Whether this row is selected or not
+   */
+  props[PROP_SELECTED] =
+    g_param_spec_boolean ("selected", "", "",
+                          FALSE,
+                          G_PARAM_READWRITE | G_PARAM_EXPLICIT_NOTIFY | G_PARAM_STATIC_STRINGS);
+
+  g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
+
+  gtk_widget_class_set_template_from_resource (widget_class,
+                                               "/mobi/phosh/plugins/"
+                                               "caffeine-quick-setting/interval-row.ui");
+
+  gtk_widget_class_bind_template_child (widget_class, PhoshIntervalRow, revealer);
+}
+
+
+static void
+phosh_interval_row_init (PhoshIntervalRow *self)
+{
+  gtk_widget_init_template (GTK_WIDGET (self));
+}
+
+
+PhoshIntervalRow *
+phosh_interval_row_new (uint value, gboolean selected)
+{
+  return g_object_new (PHOSH_TYPE_INTERVAL_ROW,
+                       "value", value,
+                       "selected", selected,
+                       NULL);
+}
+
+
+uint
+phosh_interval_row_get_value (PhoshIntervalRow *self)
+{
+  g_return_val_if_fail (PHOSH_IS_INTERVAL_ROW (self), 0);
+
+  return self->value;
+}
+
+
+void
+phosh_interval_row_set_selected (PhoshIntervalRow *self, gboolean selected)
+{
+  g_return_if_fail (PHOSH_IS_INTERVAL_ROW (self));
+
+  if (self->selected == selected)
+    return;
+
+  self->selected = selected;
+
+  g_object_notify_by_pspec (G_OBJECT (self), props[PROP_SELECTED]);
+}

plugins/caffeine-quick-setting/interval-row.h
@@ -1,0 +1,23 @@
+/*
+ * Copyright (C) 2025 The Phosh Developers
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <handy.h>
+
+#define PHOSH_INTERVAL_ROW_INFINITY_VALUE G_MAXUINT32
+
+G_BEGIN_DECLS
+
+#define PHOSH_TYPE_INTERVAL_ROW (phosh_interval_row_get_type ())
+
+G_DECLARE_FINAL_TYPE (PhoshIntervalRow, phosh_interval_row, PHOSH, INTERVAL_ROW, HdyActionRow)
+
+PhoshIntervalRow *phosh_interval_row_new (uint value, gboolean selected);
+uint              phosh_interval_row_get_value (PhoshIntervalRow *self);
+void              phosh_interval_row_set_selected (PhoshIntervalRow *self, gboolean selected);
+
+G_END_DECLS

plugins/caffeine-quick-setting/interval-row.ui
@@ -1,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<interface>
+  <requires lib="gtk+" version="3.24"/>
+  <requires lib="libhandy" version="1.8"/>
+  <template class="PhoshIntervalRow" parent="HdyActionRow">
+    <property name="visible">1</property>
+    <property name="activatable">1</property>
+    <child type="prefix">
+      <object class="GtkImage" id="icon">
+        <property name="visible">1</property>
+      </object>
+    </child>
+    <child>
+      <object class="GtkRevealer" id="revealer">
+        <property name="visible">1</property>
+        <property name="transition-type">crossfade</property>
+        <child>
+          <object class="GtkImage">
+            <property name="visible">1</property>
+            <property name="pixel-size">16</property>
+            <property name="icon-name">object-select</property>
+          </object>
+        </child>
+      </object>
+    </child>
+  </template>
+</interface>

plugins/caffeine-quick-setting/meson.build
@@ -8,6 +8,7 @@ caffeine_quick_setting_resources = gnome.compile_resources(
 
 caffeine_quick_setting_plugin_sources = files(
   'caffeine-quick-setting.c',
+  'interval-row.c',
   'phosh-plugin-caffeine-quick-setting.c',
 )
 
@@ -46,4 +47,19 @@ install_data(
   'icons/cafe-hot-symbolic.svg',
   rename: 'caffeine-quick-setting-symbolic.svg',
   install_dir: phosh_plugin_icon_dir,
+)
+
+caffeine_quick_setting_schema = 'mobi.phosh.plugins.caffeine-quick-setting.gschema.xml'
+compiled = gnome.compile_schemas(depend_files: caffeine_quick_setting_schema)
+compile_schemas = find_program('glib-compile-schemas', required: false)
+if compile_schemas.found()
+  test(
+    'Validate @0@ schema file'.format(caffeine_quick_setting_schema),
+    compile_schemas,
+    args: ['--strict', '--dry-run', meson.current_source_dir()],
+  )
+endif
+install_data(
+  caffeine_quick_setting_schema,
+  install_dir: 'share/glib-2.0/schemas',
 )

plugins/caffeine-quick-setting/mobi.phosh.plugins.caffeine-quick-setting.gschema.xml
@@ -1,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<schemalist>
+  <schema id="mobi.phosh.plugins.caffeine-quick-setting"
+          path="/mobi/phosh/plugins/caffeine-quick-setting/">
+    <key name="intervals" type="au">
+      <default>[120, 300, 900, 4294967295]</default>
+      <summary>Time intervals for which caffeine can remain on</summary>
+      <description>
+        Various time interval options, during which caffeine will stay on, in seconds.
+      </description>
+    </key>
+
+    <key name="selected-index" type="u">
+      <default>3</default>
+      <summary>Index of selected interval</summary>
+      <description>
+        Corresponding index of the selected interval. If out of bounds,
+        default value of infinity is selected.
+      </description>
+    </key>
+  </schema>
+</schemalist>

plugins/caffeine-quick-setting/phosh-plugin-caffeine-quick-setting.gresources.xml
@@ -2,6 +2,7 @@
 <gresources>
   <gresource prefix="/mobi/phosh/plugins/caffeine-quick-setting/">
     <file preprocess="xml-stripblanks">qs.ui</file>
+    <file preprocess="xml-stripblanks">interval-row.ui</file>
     <file compressed="true">icons/cafe-hot-symbolic.svg</file>
     <file compressed="true">icons/cafe-cold-symbolic.svg</file>
   </gresource>

plugins/caffeine-quick-setting/qs.ui
@@ -3,10 +3,42 @@
   <requires lib="gtk+" version="3.24"/>
   <template class="PhoshCaffeineQuickSetting" parent="PhoshQuickSetting">
     <property name="status-icon">info</property>
+    <property name="status-page">status_page</property>
     <signal name="clicked" handler="on_clicked" object="PhoshCaffeineQuickSetting" swapped="yes"/>
   </template>
   <object class="PhoshStatusIcon" id="info">
     <property name="visible">1</property>
     <property name="pixel-size">16</property>
+  </object>
+  <object class="PhoshStatusPage" id="status_page">
+    <property name="visible">1</property>
+    <property name="title" translatable="yes">Caffeine timers</property>
+    <property name="content">stack</property>
+  </object>
+  <object class="GtkStack" id="stack">
+    <property name="visible">1</property>
+    <child>
+      <object class="GtkListBox" id="listbox">
+        <property name="visible">1</property>
+        <property name="selection-mode">none</property>
+        <signal name="row-activated" handler="on_interval_row_activated" object="PhoshCaffeineQuickSetting" swapped="no"/>
+        <style>
+          <class name="content"/>
+        </style>
+      </object>
+      <packing>
+        <property name="name">listbox</property>
+      </packing>
+    </child>
+    <child>
+      <object class="PhoshStatusPagePlaceholder">
+        <property name="visible">1</property>
+        <property name="icon-name">cafe-hot-symbolic</property>
+        <property name="title" translatable="yes">No caffeine intervals</property>
+      </object>
+      <packing>
+        <property name="name">empty-state</property>
+      </packing>
+    </child>
   </object>
 </interface>

commit 4fee897ad5d0caf6a0c6a1d1db58302e90b648a1

caffeine-quick-settings: Make sure we clear the timer on finalize
Otherwise we might end up getting invoked when the widget is already
gone. clear_timer also also drops the inhibitor.

Fixes: 74e88ef6c ("caffeine: Allow to select caffeine duration")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1934>

plugins/caffeine-quick-setting/caffeine-quick-setting.c
@@ -259,9 +259,7 @@ phosh_caffeine_quick_setting_finalize (GObject *gobject)
 {
   PhoshCaffeineQuickSetting *self = PHOSH_CAFFEINE_QUICK_SETTING (gobject);
 
-  if (self->cookie)
-    phosh_caffeine_quick_setting_inhibit (self, FALSE);
-
+  phosh_caffeine_quick_setting_clear_timer (self);
   g_clear_object (&self->settings);
 
   G_OBJECT_CLASS (phosh_caffeine_quick_setting_parent_class)->finalize (gobject);