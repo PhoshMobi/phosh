PhoshMobi/phosh

commit 0df159a2c1dcb65633526e4cc4f9bd01ccabf0cc

lockscreen-bg: Make sure to use device pixels
We need to load the pixmaps at the full screen resolution, not
the logical size, otherwise they become blurry.

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Tested-by: Sorin Iclanzan
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1946>

src/lockscreen-bg.c
@@ -42,7 +42,7 @@ G_DEFINE_TYPE (PhoshLockscreenBg, phosh_lockscreen_bg, PHOSH_TYPE_LAYER_SURFACE)
 static void
 update_image (PhoshLockscreenBg *self)
 {
-  int width, height;
+  int width, height, scale;
 
   if (!self->configured)
     return;
@@ -52,12 +52,15 @@ update_image (PhoshLockscreenBg *self)
 
   g_return_if_fail (width > 0 && height > 0);
 
+  scale = gtk_widget_get_scale_factor (GTK_WIDGET (self));
+  scale = MAX (1, scale);
+
   g_debug ("Scaling lockscreen background %p to %dx%d", self, width, height);
 
   g_clear_object (&self->pixbuf);
   if (self->bg_image) {
     GdkPixbuf *pixbuf = phosh_background_image_get_pixbuf (self->bg_image);
-    self->pixbuf = phosh_utils_pixbuf_scale_to_min (pixbuf, width, height);
+    self->pixbuf = phosh_utils_pixbuf_scale_to_min (pixbuf, width * scale, height * scale);
   }
 
   gtk_widget_queue_draw (GTK_WIDGET (self));
@@ -96,7 +99,11 @@ phosh_lockscreen_bg_draw (GtkWidget *widget, cairo_t *cr)
   gtk_render_background (context, cr, 0, 0, width, height);
 
   if (self->pixbuf && self->use_background) {
-    gdk_cairo_set_source_pixbuf (cr, self->pixbuf, x, y);
+    double scale = gtk_widget_get_scale_factor (GTK_WIDGET (self));
+
+    cairo_scale (cr, 1.0 / scale, 1.0 / scale);
+
+    gdk_cairo_set_source_pixbuf (cr, self->pixbuf, x / scale, y / scale);
     cairo_paint (cr);
   }
 

commit 7b48cd71b31795c6501ad524e13a06a7bb8aec9d

lockscreen-bg: Drop pixbuf x,y coords
They're always 0 and the scale calculation was
inversed too.

Fixes: 0df159a2c ("lockscreen-bg: Make sure to use device pixels")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1949>

src/lockscreen-bg.c
@@ -84,7 +84,7 @@ phosh_lockscreen_bg_draw (GtkWidget *widget, cairo_t *cr)
 {
   GtkStyleContext *context;
   PhoshLockscreenBg *self = PHOSH_LOCKSCREEN_BG (widget);
-  int x = 0, y = 0, width, height;
+  int width, height;
 
   g_return_val_if_fail (PHOSH_IS_LOCKSCREEN_BG (self), GDK_EVENT_PROPAGATE);
 
@@ -103,7 +103,7 @@ phosh_lockscreen_bg_draw (GtkWidget *widget, cairo_t *cr)
 
     cairo_scale (cr, 1.0 / scale, 1.0 / scale);
 
-    gdk_cairo_set_source_pixbuf (cr, self->pixbuf, x / scale, y / scale);
+    gdk_cairo_set_source_pixbuf (cr, self->pixbuf, 0, 0);
     cairo_paint (cr);
   }
 